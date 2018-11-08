ae9d012a658c7e9033632933dfe9c9824076cde5
Dmitry Ryazantsev <dika1993@yandex.ru>
2018-11-08 23:48:26 +0300

    Словарь и функция shuffle перенесены на сервер, добавлены функции saveStatistics и getErrorsBtn в script.js

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
 "use strict";
 
-let dictionary = [
-	"apple", "word", "coffee", "green" , "red", "blue", "awesome", "amazing", "function", "artist", "singer", "black",
-	"five", "for", "about", "what", "restaurant", "silence", "hotel", "luxury", "woman", "cat", "dog", "sister", "mother",
-	"wallet", "pharmacy", "mountain", "box", "road", "interesting", "capital", "hope", "pain"
-];
-
-let shuffle = (array) => {
-	array.forEach((value, index) => {
-		// случайное число от 0 до индекса последнего элемента
-		let rand = Math.floor(Math.random() * array.length);
-		// вместо текущего элемента ставим случайны
-		array[index] = array[rand];
-		// а вместо случайного - текущий
-		array[rand] = value;
-	});
-};
-
-shuffle(dictionary);
-
 window.addEventListener("load", function () {
         const limitQuestions = 5;
         let currentQuestion = 0;
         let currentLetter = 0;
-
         let answerContainer = document.querySelector("#answer");
         let letters = document.querySelector("#letters");
         let currentQuestionElem = document.querySelector("#current_question");
@@ -51,6 +31,23 @@ window.addEventListener("load", function () {
             currentQuestionElem.innerHTML = currentQuestion + 1;
             answerContainer.innerHTML = "";
         };
+        let saveStatistics = () => {
+            const req = new XMLHttpRequest();
+            req.open("POST", "http://localhost:5500/save?errors=" + totalErrors);
+            req.send();
+            req.onload = () => console.log("Успешно сохранена статистика на сервере!");
+            req.onerror = () => console.log("Ошибка при отправке запроса на сохранение");
+        };
+        // этот код лучше поместить в функцию
+        // const req = new XMLHttpRequest();
+        // req.open("GET", ".....?limit=" + limitQuestions);
+        // req.send();
+        // req.onload = () => {
+        // лучше всего использовать JSON
+        // dictionary = req.responseText;
+        // // в responseText должен быть список слов
+        // // код для запуска игры (в т.ч. для показа первого вопроса)
+        // };
 
         displayCurrentQuestion();
 
@@ -61,7 +58,7 @@ window.addEventListener("load", function () {
                         window.location.reload();
                     } else if (event.target.tagName === "BUTTON") {
                         let btn = event.target;
-                let word = dictionary[currentQuestion];
+                        let word = dictionary[currentQuestion];
 
                 // если текст на кнопке совпадает с буквой (текущей) в текущем слове
                 if (btn.innerHTML === word[currentLetter]) {
@@ -90,6 +87,7 @@ window.addEventListener("load", function () {
                             button.classList.add("play");
                             button.innerHTML = "Играть снова?";
                             letters.appendChild(button);
+                            saveStatistics();
 
                         } else {
                             displayCurrentQuestion();
@@ -112,6 +110,19 @@ window.addEventListener("load", function () {
             }
                 
             }
+        });
+
+        getErrorsBtn.addEventListener("click", function () {
+            const req = new XMLHttpRequest();
+            req.open("GET", "http://localhost:5500/stat");
+            req.onload = () => {
+                if (req.status === 200) {
+                    alert("Всего ошибок у всех пользователей: " + req.responseText);
+                } else {
+                    alert("Не удалось получить статистику!");
+                }
+            };
+            req.send();
         });
     });
 
